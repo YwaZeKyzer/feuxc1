06c7bcb6b8722eabebc613ae3d834fc1f84ed972
YwaZeKyzer <58531549+YwaZeKyzer@users.noreply.github.com>
2022-12-05 19:58:00 +0100

    creation d'un decompte entre feu rouge et feu vert

--- main.ts
+++ main.ts
@@ -1,36 +1,31 @@
+let t = 0
 let strip: neopixel.Strip = null
 let etat = 0
+function countdown () {
+    while (t > 0) {
+        basic.showNumber(t)
+        t += -1
+        basic.pause(200)
+    }
+    basic.clearScreen()
+    strip.clear()
+    strip.show()
+    feuV()
+}
 function feuV () {
+    etat = 0
     strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
     strip.show()
     basic.pause(10000)
     strip.clear()
     strip.show()
 }
-function sousprogramme () {
-    if (etat == 0) {
-        feuV()
-        strip.clear()
-        strip.show()
-        etat = 1
-    } else if (etat == 1) {
-        FeuO()
-        strip.clear()
-        strip.show()
-        etat = 2
-    } else {
-        feuR()
-        strip.clear()
-        strip.show()
-        etat = 0
-    }
-}
 function feuR () {
     strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
     strip.show()
-    basic.pause(12000)
-    strip.clear()
-    strip.show()
+    if (etat == 1) {
+        countdown()
+    }
 }
 function FeuO () {
     strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Orange))
@@ -42,5 +37,7 @@ function FeuO () {
 basic.forever(function () {
     strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
     strip.setBrightness(32)
-    sousprogramme()
+    t = 10
+    etat = 1
+    feuR()
 })
